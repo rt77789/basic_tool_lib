da45fa080c18005e0b54a1886dfe0ef124cf50c2
郑毅 <rt77789@163.com>
2012-06-08 16:21:23 +0800

    add test/main.cp

---
+++ test/main.cpp
@@ -0,0 +1,20 @@
+
+#include <util.h>
+#include <hash.h>
+
+int main() {
+using namespace eoaix;
+	HashMap<TestKey, TestKey> htable;
+
+	TestKey t1(1, "1str");
+	TestKey t2(2, "2str");
+
+	//htable.insert(t1, t2);
+	//htable.insert(t2, t1);
+
+	//TestKey* t3 = htable.find_val(t1);
+	
+	//std::cout << t3->toString() << std::endl;
+
+	return 0;
+}
